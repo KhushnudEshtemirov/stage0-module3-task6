d96c63b8d4df0eaa73c06d3b9bfe0b61888555b2
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-18 17:31:10 +0500

    Finished task

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        double first = 1000;
+        int second = (int) first;
+        System.out.println(first + "\n" + second);
     }
 }
